da28a94c1d11bd6b14cb2d65cea1bf8a386f7fd6
hieu <trunghieu27102002@gmail.com>
2024-03-23 15:44:46 +0700

    change api url 9

--- service/landing-page.service.ts
+++ service/landing-page.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class LandingPageService {
 // private urlApi= 'http://localhost:3000';
 private urlApi = 'https://backend-admin-vimi.vercel.app' // deploy vercel
-// private urlApi = 'https://vimi.vn:8011' // deploy server
+// private urlApi = 'http://vimi.vn:8011' // deploy server
   constructor(private http: HttpClient) { }
 
     sendInforUser(inforUser: {
